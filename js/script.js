dbc88071ceaf9d7e66e8494f344d927f036c20d6
sushildhillon <sushildhillon@outlook.com>
2020-05-27 16:23:06 +0100

    a

--- js/script.js
+++ js/script.js
@@ -1,24 +1,18 @@
-
-    $(document).ready(function () {
-        $("#sidebar").mCustomScrollbar({
-            theme: "minimal"
-        });
-
-        $('#dismiss, .overlay').on('click', function () {
-            // hide sidebar
-            $('#sidebar').removeClass('active');
-            // hide overlay
-            $('.overlay').removeClass('active');
-        });
-
-        $('#sidebarCollapse').on('click', function () {
-            // open sidebar
-            $('#sidebar').addClass('active');
-            // fade in the overlay
-            $('.overlay').addClass('active');
-            $('.collapse.in').toggleClass('in');
-            $('a[aria-expanded=true]').attr('aria-expanded', 'false');
-        });
-    });
-
-
+$(document).ready(function() {
+  var fixHeight = function() {
+    $('.navbar-nav').css(
+      'max-height',
+      document.documentElement.clientHeight - 150
+    );
+  };
+  fixHeight();
+  $(window).resize(function() {
+    fixHeight();
+  });
+  $('.navbar .navbar-toggler').on('click', function() {
+    fixHeight();
+  });
+  $('.navbar-toggler, .overlay').on('click', function() {
+    $('.mobileMenu, .overlay').toggleClass('open');
+  });
+});
